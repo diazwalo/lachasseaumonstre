d5c0485aa7436f98af69f66525c4b7eb141b2625
diazw <diazw@DESKTOP-UNOJQ8K>
2019-03-23 19:19:55 +0100

    Création de l'énum Mouvement pour les déplacements
    Mouvement: +attributs mvtX et mvtY
    Mouvement: +getMvtX et getMvtYpour avoir le deplacements sur l'une des deux dimentions
    Mouvement: +getMvt qui retourne le mouvement sous la forme d'un tableau à 2 dimentions

---
+++ src/map/Mouvement.java
@@ -0,0 +1,24 @@
+package map;
+
+public enum Mouvement {
+	NORD(0,1), SUD(0, -1), EST(1, 0), OUEST(-1, 0), NORDEST(1,1), NORDOUEST(-1, 1), SUDEST(1, -1), SUDOUEST(-1, -1);
+	private int mvtX;
+	private int mvtY;
+	
+	private Mouvement(int mvtX, int mvtY) {
+		this.mvtX=mvtX;
+		this.mvtY=mvtY;
+	}
+	
+	public int getMvtX() {
+		return this.mvtX;
+	}
+	
+	public int getMvtY() {
+		return this.mvtY;
+	}
+	
+	public int[][]getMvt() {
+		return new int[][] {{this.mvtX}, {this.mvtY}};
+	}
+}
