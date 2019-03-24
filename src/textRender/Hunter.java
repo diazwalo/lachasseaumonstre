af6b7aa35b8dacdd2bb56d853b0edad83cf8d458
Quentin Prognon <Quentin Prognon@LAPTOP-S1KU7V4R>
2019-03-24 21:15:11 +0100

    Doc pour Hunter et Beast

--- src/textRender/Hunter.java
+++ src/textRender/Hunter.java
@@ -1,5 +1,9 @@
 package textRender;
-
+/**
+ * 
+ * @author Quentin Prognon
+ *
+ */
 public class Hunter implements Entity {
 	private int posX;
 	private int posY;
@@ -8,31 +12,47 @@ public class Hunter implements Entity {
 		this.posX=posX;
 		this.posY=posY;
 	}
-
+/**
+ * Renvoie un boolean qui deternime si la bete est presente au coordonnées passer en paramètre,
+ */
 	public boolean estSurCase(int posX, int posY) {
 		return this.posX==posX && this.posY==posY;
 	}
-	
+	/**
+	 * Renvoie la position en X du chasseur
+	 * @see textRender.Entity#getPosY()
+	 */
 	public int getPosX() {
 		return this.posX;
 	}
-
+	/**
+	 * Renvoie la position en y du chasseur
+	 * @see textRender.Entity#getPosY()
+	 */
 	public int getPosY() {
 		return this.posY;
 	}
-	
+	/**
+	 * Edite la potion en X du chasseur
+	 */
 	public void setPosX(int posX) {
 		this.posX=posX;
 	}
-	
+	/**
+	 * Edite la potion en X du chasseur
+	 */
 	public void setPosY(int posY) {
 		this.posY=posY;
 	}
-	
+	/**
+	 * Renvois un tableau d'entier contenant les coordonées du chasseur sous la forme (coord x , coord y)
+	 */
 	public int[] position() {
 		return new int[] {posX ,posY};
 	}
-	
+	/**
+	 * Renvoie un H
+	 */
 	public String toString() {
 		return "H";
 	}
