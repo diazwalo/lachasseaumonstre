199e73487387833d40c881d610890a94be248067
Virgil <virgil031299@gmail.com>
2019-03-26 15:00:47 +0100

    Remplacement de l'interface Entity par la Super Class éponyme et création de la Class Position
    Beast: ~
    Hunter: ~

--- src/map/Mouvment.java
+++ src/map/Mouvment.java
@@ -2,17 +2,17 @@ package map;
 
 /**
  * Cette classe defini tous les mouvements possible pour les joueurs
- * (bête ou chasseurs)
+ * (bete ou chasseurs)
  * @author nath
  *
  */
 
-public enum Mouvement {
+public enum Mouvment {
 	NORD(0,1), SUD(0, -1), EST(1, 0), OUEST(-1, 0), NORDEST(1,1), NORDOUEST(-1, 1), SUDEST(1, -1), SUDOUEST(-1, -1);
 	private int mvtX;
 	private int mvtY;
 	
-	private Mouvement(int mvtX, int mvtY) {
+	private Mouvment(int mvtX, int mvtY) {
 		this.mvtX=mvtX;
 		this.mvtY=mvtY;
 	}
