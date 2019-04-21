bfa7386ce689df0bd7dfafa7aa3196e86e83a1ca
diazw <diazw@DESKTOP-UNOJQ8K>
2019-04-21 23:48:09 +0200

    Creation de GameStatus

---
+++ src/core/game/GameStatus.java
@@ -0,0 +1,15 @@
+package core.game;
+
+public enum GameStatus {
+	INGAME("partie en cours"), FOUND("trouve par le chasseur"), DISCOVERED("la map a ete entierement decouverte par la bete"), BLOCK("l'adversaire est bloque");
+	
+	private String status;
+	
+	private GameStatus(String status) {
+		this.status=status;
+	}
+	
+	public String getStatus() {
+		return this.status;
+	}
+}
---
+++ src/core/game/IGame.java
@@ -0,0 +1,5 @@
+package core.game;
+
+public interface IGame {
+	
+}
