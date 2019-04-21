ea0db07b1bc7790059b03196ed86c826e5703c49
nath <nath@nath-PC>
2019-04-22 01:06:54 +0200

    ajout des fonction dans l'interface IGame

--- src/core/game/IGame.java
+++ src/core/game/IGame.java
@@ -2,4 +2,12 @@ package core.game;
 
 public interface IGame {
 	
+	public void launchGame();
+	public void checkGameStatus();
+	public boolean statusFound();
+	public boolean statusDiscovered();
+	public boolean statusEnemyblock();
+	public boolean beastTurn();
+	public boolean hunterTurn();
+	
 }
