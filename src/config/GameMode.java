f8d24724a8000488bbc3546f13fb01d7ff2979d4
PHPierre <meganium.151@gmail.com>
2019-04-22 17:08:36 +0200

    Document GameMode enum

--- src/config/GameMode.java
+++ src/config/GameMode.java
@@ -1,5 +1,14 @@
 package config;
 
+/**
+ * Cette enumeration liste les modes de jeu disponible aux utilisateurs.
+ * AIvsAI        : L'ordinateur joue contre lui meme et controle les deux entites.
+ * BEASTvsAI     : Le joueur controle le monstre et l'ordinateur controle le chasseur.
+ * HUNTERvsAI    : Le joueur controle le chasseur et l'ordinateur controle le monstre.
+ * BEASTvsHUNTER : Un des deux joueurs controle le monstre et son adversaire controle le chasseur.
+ * @author PHPierre
+ *
+ */
 public enum GameMode {
 	AIvsAI,
 	BEASTvsAI,
