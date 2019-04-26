cb6b3217f977099f10082c0361916962c89aa19a
Pierre MAYEUX <mayeuxp@teck26.iut-infobio.priv.univ-lille1.fr>
2019-04-26 16:24:15 +0200

    Edit CommandParser test

--- test/src/core/CommandParserTest.java
+++ test/src/core/CommandParserTest.java
@@ -1,6 +1,7 @@
 package src.core;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -67,7 +68,7 @@ public class CommandParserTest
 		assertEquals(commandParser5.getConfig().getGameMode(), GameMode.BEASTvsHUNTER);
 	}
 
-	/*@Test
+	@Test
 	public void testArgsNbTeleportation()
 	{
 		String[] cmd0 = {};
@@ -81,7 +82,7 @@ public class CommandParserTest
 		assertEquals(commandParser0.getConfig().getNbTeleportation(), 3);
 		assertEquals(commandParser1.getConfig().getNbTeleportation(), 42);
 		assertEquals(commandParser2.getConfig().getNbTeleportation(), 42);
-	}*/
+	}
 	
 	@Test
 	public void testArgsTrap()
@@ -161,6 +162,7 @@ public class CommandParserTest
 		assertEquals(42, commandParser0.getConfig().getWidth());
 		assertEquals(42, commandParser0.getConfig().getHeight());
 		assertEquals(GameMode.AIvsAI, commandParser0.getConfig().getGameMode());
+		assertEquals(3, commandParser0.getConfig().getNbTeleportation());
 		assertTrue(commandParser0.getConfig().isWard());
 		assertTrue(commandParser0.getConfig().isTrap());
 		assertTrue(commandParser0.getConfig().isCamouflage());
