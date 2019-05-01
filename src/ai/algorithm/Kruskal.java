fe0f8054b4004b90af42fd4a3cb296b80e64d030
PHPierre <meganium.151@gmail.com>
2019-05-01 02:30:46 +0200

    Add Kruskal algorithm (not tested)

--- src/ai/algorithm/Kruskal.java
+++ src/ai/algorithm/Kruskal.java
@@ -8,6 +8,7 @@ import java.util.TreeMap;
 import ai.comparator.EdgeComparator;
 import ai.graph.Graph;
 import ai.models.Edge;
+import ai.models.Node;
 import map.Position;
 
 /**
@@ -15,16 +16,21 @@ import map.Position;
  * Elle permet de trouver le chemin qui necessite un minimun de temps pour le parcourir
  * tout en explorant toute la map.
  * 
+ * Sources :
+ * https://fr.wikipedia.org/wiki/Algorithme_de_Kruskal
+ * https://fr.wikipedia.org/wiki/Union-find
  * @author PHPierre
  *
  */
 public class Kruskal
 {
 
+	private Map<String, Node> listNode;
 	private Map<String, Edge> listEdge;
 	
 	public Kruskal(Graph graph)
 	{
+		this.listNode = graph.getListNode();
 		this.listEdge = graph.getListEdge();
 	}
 	
@@ -42,5 +48,36 @@ public class Kruskal
 		EdgeComparator edgeComparator = new EdgeComparator(this.listEdge);
 		Map<String, Edge> edgeList = new TreeMap<String, Edge>(edgeComparator);
 
+		List<Edge> graphEdge = new ArrayList<>();
+		
+		for (String name : this.listNode.keySet()) {
+			this.listNode.get(name).setPrecedent(null);
+		}
+		
+		for (String nameEdge : edgeList.keySet()) {
+			Edge edge = this.listEdge.get(nameEdge);
+			if(this.find(edge.getNodeOneName()) != this.find(edge.getNodeTwoName())) {
+				graphEdge.add(edge);
+				this.union(edge.getNodeOneName(), edge.getNodeTwoName());
+			}
+		}
+	}
+	
+	private String find(String name)
+	{
+		if(this.listNode.get(name).getPrecedent() == null) {
+			return name;
+		}
+		return this.listNode.get(name).getPrecedent();
+	}
+	
+	private void union(String x, String y)
+	{
+		this.listNode.get(x).setPrecedent(this.find(x));
+		this.listNode.get(y).setPrecedent(this.find(y));
+		
+		if(this.listNode.get(x) != this.listNode.get(y)) {
+			this.listNode.get(x).setPrecedent(y);
+		}
 	}
 }
