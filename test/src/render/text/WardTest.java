b40e6f97a0ab7869d3c7267e0e09e0a14101aefb
Quentin Prognon <Quentin Prognon@LAPTOP-S1KU7V4R>
2019-05-01 18:52:49 +0200

    + Test Bonus

---
+++ test/src/render/text/WardTest.java
@@ -0,0 +1,50 @@
+package src.render.text;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import map.Position;
+import render.text.Bonus;
+import render.text.Ward;
+
+public class WardTest {
+	
+	private  Ward balise ;
+	
+
+	@Before 
+	public  void beforeTest(){
+		balise = new Ward();
+	}
+
+	
+	@Test 
+	public void testInstall() {
+		assertNull(balise.getPos());
+		balise.install(5, 5);
+		assertTrue(balise.getPos().equals(new Position(5,5)));
+		balise.install(5, 6);
+		assertTrue(balise.getPos().equals(new Position(5,6)));
+	}
+	
+	@Test
+	public void testRefresh() {
+		balise.install(5, 5);
+		balise.refresh();
+		assertEquals(4, balise.getTimer());
+		assertNotNull(balise.getPos());
+		balise.refresh();
+		assertEquals(3, balise.getTimer());
+		balise.refresh();
+		assertEquals(2, balise.getTimer());
+		balise.refresh();
+		assertEquals(1, balise.getTimer());
+		balise.refresh();
+		assertEquals(0, balise.getTimer());
+		assertNull(balise.getPos());
+	}
+
+}
