52d6377a9f642a7cc3d87059529d290eab5c0538
Quentin Prognon <Quentin Prognon@LAPTOP-S1KU7V4R>
2019-05-01 22:05:41 +0200

    +Camouflage et les test

--- src/render/bonus/Camouflage.java
+++ src/render/bonus/Camouflage.java
@@ -1,6 +1,62 @@
 package render.bonus;
 
-public class Camouflage {
-	// a completer
-	boolean test;
+import map.Position;
+
+/**
+ * @author prognonq
+ *
+ */
+public class Camouflage implements IBonus {
+	
+	private Position pos;
+	private final String NAME = "Piege";
+
+	/**
+	 * Instancie Camouflage
+	 */
+	public Camouflage() {
+		this.pos = null;
+	}
+	
+	/**
+	 * place ce Bonus a la position d abscice x et d ordonnee y
+	 */
+	@Override
+	public void install(int x, int y) {
+		this.pos = new Position(x,y);
+	}
+	
+	/**
+	 * place ce Bonus a la position données
+	 */
+	public void install(Position pos) {
+		this.pos = pos;
+	}
+	
+	
+	/**
+	 * passe la position du camouflage a null
+	 */
+	public void unistall() {
+		this.pos = null;
+	}
+	
+	/**
+	 * retourne la position du camouflage
+	 * @return
+	 */
+
+	public Position getPos() {
+		return pos;
+	}
+
+	/**
+	 * retourne le nom
+	 * @return
+	 */
+
+	@Override
+	public String getName() {
+		return this.NAME;
+	}
 }
