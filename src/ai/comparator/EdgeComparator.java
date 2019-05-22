7cc9f8d4c394d162f0f90eea367c1eab6f62f6bb
Pierre MAYEUX <mayeuxp@ayou22.iut-infobio.priv.univ-lille1.fr>
2019-05-22 15:30:11 +0200

    Update doc

--- src/ai/comparator/EdgeComparator.java
+++ src/ai/comparator/EdgeComparator.java
@@ -5,11 +5,9 @@ import ai.models.EdgeTmp;
 
 public class EdgeComparator implements Comparator<EdgeTmp>
 {
-
 	@Override
 	public int compare(EdgeTmp o1, EdgeTmp o2) {
 		return o1.getWeight() - o2.getWeight();
 	}
-
 
 }
--- src/ai/exception/NodeNotFoundException.java
+++ src/ai/exception/NodeNotFoundException.java
@@ -1,9 +1,17 @@
 package ai.exception;
 
+/**
+ * @author mayeuxp
+ *
+ */
 public class NodeNotFoundException extends RuntimeException
 {
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Cette fonction affiche une erreur si un accès à un sommet inconnu est rencontré
+	 * @param nodeName L'identifiant du sommet.
+	 */
 	public NodeNotFoundException(String nodeName)
 	{
 		System.out.println("Le sommet "+ nodeName + " n'existe pas sur le plateau de jeu.");
