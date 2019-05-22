a760ef73478eb2f5b8c807c9e87c622de18c858a
Pierre MAYEUX <mayeuxp@ayou22.iut-infobio.priv.univ-lille1.fr>
2019-05-22 16:32:00 +0200

    Add Widget home

---
+++ src/render/ui/component/Home.java
@@ -0,0 +1,23 @@
+package render.ui.component;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class Home
+{
+	private HBox core;
+	private VBox menu;
+	
+	public Home()
+	{
+		this.core = new HBox();
+		this.menu = new VBox();
+		//Image imageBackground = new Image(url);
+		//ImageView background = new ImageView();
+		
+		//background.setImage(new Image());
+	
+	}
+}
