795b0acb5016b424229c6bf645b99c7d0d856a08
prognonq <prognonq@ayou10.iut-infobio.priv.univ-lille1.fr>
2019-05-29 15:18:34 +0200

    ~playButton

--- src/render/ui/form/button/PlayButton.java
+++ src/render/ui/form/button/PlayButton.java
@@ -8,32 +8,58 @@ public class PlayButton {
 	
 	private VBox core;
 	private HBox topside;
+	private HBox middle;
 	private HBox bottom;
 	
 	private KeyboardButton topBtn;
 	private KeyboardButton leftBtn;
 	private KeyboardButton rightBtn;
 	private KeyboardButton bottomBtn;
+	private KeyboardButton upRightBtn;
+	private KeyboardButton upLeftBtn;
+	private KeyboardButton bottomRightBtn;
+	private KeyboardButton bottomLeftBtn;
 	
 	public PlayButton() {
 		this.core = new VBox();
 		this.topside = new HBox();
 		this.bottom = new HBox();
+		this.middle = new HBox();
 		
 		this.topBtn  = new KeyboardButton("z");
 		this.leftBtn  = new KeyboardButton("q");
 		this.rightBtn  = new KeyboardButton("d");
 		this.bottomBtn  = new KeyboardButton("s");
+		this.upRightBtn = new KeyboardButton("zd");
+		this.upLeftBtn = new KeyboardButton("zq");
+		this.bottomRightBtn = new KeyboardButton("sd");
+		this.bottomLeftBtn = new KeyboardButton("sq");
+		
+		
 		
 		this.topside.setPadding(new Insets(0, 50, 0, 50));
 		
-		this.topside.getChildren().add(this.topBtn.getBouton());
-		this.bottom.getChildren().addAll(
-			this.leftBtn.getBouton(),
-			this.bottomBtn.getBouton(),
-			this.rightBtn.getBouton()
+		this.topside.getChildren().addAll(
+			this.upLeftBtn.getBouton(),
+			this.topBtn.getBouton(),
+			this.upRightBtn.getBouton()
 		);
+		
+		this.middle.getChildren().addAll(
+				this.leftBtn.getBouton(),
+				this.rightBtn.getBouton()
+				
+			);
+	
+		this.bottom.getChildren().addAll(
+			this.bottomLeftBtn.getBouton(),
+			this.bottomBtn.getBouton(),
+			this.bottomRightBtn.getBouton()
+		);
+		
+		
 		this.core.getChildren().add(topside);
+		this.core.getChildren().add(middle);
 		this.core.getChildren().add(bottom);
 	}
 
@@ -46,6 +72,10 @@ public class PlayButton {
 		leftBtn.getBouton().setMaxSize(maxWidth, maxHeight);
 		rightBtn.getBouton().setMaxSize(maxWidth, maxHeight);
 		bottomBtn.getBouton().setMaxSize(maxWidth, maxHeight);
+		bottomLeftBtn.getBouton().setMaxSize(maxWidth, maxHeight);
+		bottomRightBtn.getBouton().setMaxSize(maxWidth, maxHeight);
+		upRightBtn.getBouton().setMaxSize(maxWidth, maxHeight);
+		upLeftBtn.getBouton().setMaxSize(maxWidth, maxHeight);
 	}
 	
 	public void setMinSize(int minWidth, int  minHeight ) {
@@ -53,6 +83,11 @@ public class PlayButton {
 		leftBtn.getBouton().setMaxSize(minWidth, minHeight );
 		rightBtn.getBouton().setMaxSize(minWidth, minHeight );
 		bottomBtn.getBouton().setMaxSize(minWidth, minHeight );
+		bottomLeftBtn.getBouton().setMaxSize(minWidth, minHeight );
+		bottomRightBtn.getBouton().setMaxSize(minWidth, minHeight );
+		upRightBtn.getBouton().setMaxSize(minWidth, minHeight );
+		upLeftBtn.getBouton().setMaxSize(minWidth, minHeight );
+		
 	}
 	
 	public void setPrefSize(int prefWidth, int  prefHeight ) {
@@ -60,6 +95,10 @@ public class PlayButton {
 		leftBtn.getBouton().setMaxSize(prefWidth, prefHeight );
 		rightBtn.getBouton().setMaxSize(prefWidth, prefHeight );
 		bottomBtn.getBouton().setMaxSize(prefWidth, prefHeight );
+		bottomLeftBtn.getBouton().setMaxSize(prefWidth, prefHeight );
+		bottomRightBtn.getBouton().setMaxSize(prefWidth, prefHeight );
+		upRightBtn.getBouton().setMaxSize(prefWidth, prefHeight );
+		upLeftBtn.getBouton().setMaxSize(prefWidth, prefHeight );
 	}
 	
 	
