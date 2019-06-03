f1dfc675ea64aae12295fbbcf2c8faa093cdde40
Quentin Prognon <Quentin Prognon@LAPTOP-S1KU7V4R>
2019-06-04 00:59:04 +0200

    ~GameBoard

--- src/render/ui/component/GameBoard.java
+++ src/render/ui/component/GameBoard.java
@@ -44,9 +44,9 @@ public class GameBoard {
 		/**
 		 * pour voir si on peut afficher corectement les bonus actifs
 		 */
-		
+
 		// TODO : tout ca est a deplacer dans les instances respective (beast, hunter, les diff bonus, l'enum CaseType)
-		
+
 		FileInputStream is = new FileInputStream(new File(Directory.getGameGround()));
 		ground = new Image(is);
 		is  = new FileInputStream(new File(Directory.getGameObstacle()));
@@ -57,14 +57,14 @@ public class GameBoard {
 		hunter = new Image(is);
 		is = new FileInputStream(new File(Directory.getGameBonus()));
 		bonus = new Image(is);
-		
+
 		is = new FileInputStream(new File(Directory.getGameTrap()));
 		trap = new Image(is);
 		is = new FileInputStream(new File(Directory.getGameWard()));
 		ward = new Image(is);
 		is = new FileInputStream(new File(Directory.getGameBait()));
 		bait = new Image(is);
-		
+
 		is = new FileInputStream(new File(Directory.getGameTraceUn()));
 		traceUn = new Image(is);
 		is = new FileInputStream(new File(Directory.getGameTraceDeux()));
@@ -178,8 +178,8 @@ public class GameBoard {
 			}
 		}
 
-	
-		
+
+
 	}
 
 	public GridPane getGrid() {
@@ -198,4 +198,107 @@ public class GameBoard {
 		grid.setPrefSize(prefWidth, prefHeight);
 	}
 
+
+	public void refreshHunterView(AbstractMap map) {
+		for (int row = 0; row < (map.getTab().length); row++) {
+			for (int col = 0; col < (map.getTab()[row].length); col++) {
+				Rectangle rec = new Rectangle();
+				rec.setWidth(50);
+				rec.setHeight(50);
+
+				Case caseCour = map.getTab()[row][col];
+				Position posCase= new Position(row, col);
+
+				map.getHunter().getMvtToEmptyCase(map.getTab());
+
+			}
+		}	
+	}
+
+
+
+	private void paintRectangle(Rectangle rec , Case caseCour , AbstractMap map, int row , int col, Position posCase) {
+		if(caseCour.isObstacle()) {
+			rec.setFill(new ImagePattern(obstacle));
+		}else if(caseCour.getCaseType().equals(CaseType.SOL)) {
+			rec.setFill(new ImagePattern(ground));
+		}
+
+		if(caseCour.getHunterBonusActifOnCase(map, posCase) != null) {
+			//afficher la texture du bonus actif
+			IBonus bonusActif = caseCour.getHunterBonusActifOnCase(map, posCase);
+			if(bonusActif instanceof Trap) {
+				rec.setFill(new ImagePattern(trap));
+			}else if(bonusActif instanceof Ward) {
+				rec.setFill(new ImagePattern(ward));
+			}
+		}
+
+		if(caseCour.getBaitOnCase(map, posCase) != null) {
+			//rec.setFill(new ImagePattern(bait));
+			// afficher le Bait
+			Bait baitOnCase = (Bait) (caseCour.getBaitOnCase(map, posCase));
+			int cptBait = baitOnCase.getCount();
+			Image[] tabImage = new Image[] {traceUn, traceDeux, traceTrois, traceQuatre};
+			int[] tabCptBait = new int[] {1, 2, 3, 4};
+			for (int i = 0; i < tabCptBait.length; i++) {
+				if(tabCptBait[i] == cptBait) {
+					rec.setFill(new ImagePattern(tabImage[i]));
+				}
+			}
+		}
+
+		if(caseCour.bonusOnCase(caseCour.getBonusHunter())) {
+			//afficher un ?
+			rec.setFill(new ImagePattern(bonus));
+		}
+
+		if(caseCour.getBeastWalkNearHunter(map, posCase) != -1) {
+			//afficher les pas de la beast
+			rec.setFill(new ImagePattern(ground));
+		}
+
+		if(caseCour.getBeastNearHunter(map, posCase) != null) {
+			//afficher la beast
+			rec.setFill(new ImagePattern(beast));
+		}
+
+		if(caseCour.getHunterOnCase(map, posCase)) {
+			//affiche le Hunter
+			rec.setFill(new ImagePattern(hunter));
+		}
+
+		if(caseCour.getBeastIfRevealed(map, posCase) != null) {
+			//affiche la Beast
+			rec.setFill(new ImagePattern(beast));
+		}
+
+		if(caseCour.getEntityOnSameCase(map, posCase) != null) {
+			// afficher une image du chasseur attrapant la bete ou juste le chasseur
+			rec.setFill(new ImagePattern(hunter));
+		}
+		if(map.getBeast().getPos().isPos(row, col)) {
+			rec.setFill(new ImagePattern(beast));
+		}
+
+	}
+
+
+	public void refreshBeastView(AbstractMap map) {
+		for (int row = 0; row < (map.getTab().length); row++) {
+			for (int col = 0; col < (map.getTab()[row].length); col++) {
+				Rectangle rec = new Rectangle();
+				rec.setWidth(50);
+				rec.setHeight(50);
+
+				
+				Case caseCour = map.getTab()[row][col];
+				Position posCase= new Position(row, col);
+				this.paintRectangle(rec, caseCour, map, row, col, posCase);
+				GridPane.setRowIndex(rec, row);
+				GridPane.setColumnIndex(rec, col);
+				grid.getChildren().addAll(rec);
+			}
+		}
+	}
 }
