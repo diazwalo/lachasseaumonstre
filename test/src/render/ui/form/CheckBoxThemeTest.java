a8c44dbded5571011fd42f97601f1908e85babac
PHPierre <meganium.151@gmail.com>
2019-06-04 18:27:51 +0200

    Unit test on javafx

---
+++ test/src/render/ui/form/CheckBoxThemeTest.java
@@ -0,0 +1,26 @@
+package src.render.ui.form;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import javafx.embed.swing.JFXPanel;
+import render.ui.form.box.CheckBoxTheme;
+
+public class CheckBoxThemeTest
+{
+
+	@Test
+	public void test()
+	{
+		new JFXPanel();
+		CheckBoxTheme checkBox = new CheckBoxTheme(false);
+		
+		assertFalse(checkBox.isSelected());
+		assertEquals("-fx-font-family: 'Press Start 2P', cursive; -fx-font-size: 20px; -fx-text-fill: #eee;", checkBox.getStyle());
+		
+		checkBox = new CheckBoxTheme(true);
+		assertTrue(checkBox.isSelected());
+	}
+
+}
