aecf9e2755c96401e697c3d14a7595b29f843b4d
nath <nath@nath-PC>
2019-06-12 21:12:03 +0200

    test circular ok

--- test/src/map/CircularMapTest.java
+++ test/src/map/CircularMapTest.java
@@ -17,8 +17,6 @@ import render.text.Hunter;
 public class CircularMapTest {
 
 	public AbstractMap cm;
-	public Entity beast;
-	public Entity hunter;
 	private Config config;
 	
 	
@@ -30,14 +28,12 @@ public class CircularMapTest {
         
         this.cm = new CircularMap(this.config);
         this.cm.generationMap();
-        this.beast = new Beast(18, 15, new Config());
-		this.hunter = new Hunter(1, 4, new Config());
 	}
 	
 	@Test
 	public void verifPlacementEntity() {
-		assertEquals(new Position(18,15),cm.getBeast().getPos() );
-		assertEquals(new Position(1,4), cm.getHunter().getPos() );
+		assertEquals(new Position(19,14),cm.getBeast().getPos() );
+		assertEquals(new Position(0,5), cm.getHunter().getPos() );
 	}
 	
 	@Test
@@ -60,36 +56,38 @@ public class CircularMapTest {
 	@Test
 	public void testMoveBeast() {
 		assertFalse(cm.moveBeast(Mouvment.SUD));
-		assertArrayEquals(cm.getBeast().getPos().getTabPosition(), this.beast.getPos().getTabPosition());
+		assertEquals(cm.getBeast().getPos(), new Position(19,14));
 
 		assertFalse(cm.moveBeast(Mouvment.EST));
-		assertArrayEquals(cm.getBeast().getPos().getTabPosition(), this.beast.getPos().getTabPosition());
+		assertEquals(cm.getBeast().getPos(), new Position(19,14));
 
 		assertTrue(cm.moveBeast(Mouvment.NORD));
-		this.beast.setPos(Mouvment.NORD);
-		assertArrayEquals(cm.getBeast().getPos().getTabPosition(), this.beast.getPos().getTabPosition());
+		assertEquals(cm.getBeast().getPos(), new Position(19,13));
 
-		
-		assertFalse(cm.moveBeast(Mouvment.OUEST));
-		assertArrayEquals(cm.getBeast().getPos().getTabPosition(), this.beast.getPos().getTabPosition());
+	
+		assertTrue(cm.moveBeast(Mouvment.OUEST));
+		assertEquals(cm.getBeast().getPos(), new Position(18,13));
 		
 	}
 	
 
 	@Test
 	public void testMoveHunter() {
-		assertFalse(cm.moveHunter(Mouvment.NORD));
-		assertArrayEquals(this.cm.getHunter().getPos().getTabPosition(), this.hunter.getPos().getTabPosition());
-
-		assertFalse(cm.moveHunter(Mouvment.OUEST));
-		assertArrayEquals(this.cm.getHunter().getPos().getTabPosition(), this.hunter.getPos().getTabPosition());
-
 		assertTrue(cm.moveHunter(Mouvment.SUD));
-		this.hunter.setPos(Mouvment.SUD);
-		assertArrayEquals(this.cm.getHunter().getPos().getTabPosition(), this.hunter.getPos().getTabPosition());
+		assertEquals(cm.getHunter().getPos(), new Position(0,6));
 
-		assertFalse(cm.moveHunter(Mouvment.EST));
-		assertArrayEquals(this.cm.getHunter().getPos().getTabPosition(), this.hunter.getPos().getTabPosition());
+		assertTrue(cm.moveHunter(Mouvment.EST));
+		assertEquals(cm.getHunter().getPos(), new Position(1,6));
+		
+		assertTrue(cm.moveHunter(Mouvment.NORD));
+		assertEquals(cm.getHunter().getPos(), new Position(1,5));
+		
+		assertTrue(cm.moveHunter(Mouvment.OUEST));
+		assertEquals(cm.getHunter().getPos(), new Position(0,5));
+		
+		assertFalse(cm.moveHunter(Mouvment.NORD));
+		assertEquals(cm.getHunter().getPos(), new Position(0,5));
+		
 	}
 	
 }
